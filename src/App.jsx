748d4dbd58d1b5863f4fd76758d4162c8da8824c
Mikko De Los Reyes <mikkode212@gmail.com>
2024-07-11 12:32:59 -0400

    project clean up, font import, started navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <p>WASSAP</p>;
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <h1>Where in the world?</h1>
+      <h4>Dark Mode</h4>
+    </div>
+  )
+}
+
+export default Navbar
