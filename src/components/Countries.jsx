d1651a328cba72f3e9c2531c548e1c48bf1d6398
Mikko De Los Reyes <mikkode212@gmail.com>
2024-07-16 13:33:19 -0400

    added dropdown for filter. needs further styling

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './Countries.css';
 import './SearchBar'
 import SearchBar from './SearchBar';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const options = [
+  'one', 'two', 'three'
+];
 
 function Countries() {
   return (
     <div className='container'>
       <SearchBar/>
+      <Dropdown className='dropdown' options={options} placeholder="Select an option"/>
     </div>
   )
 }
