8d20beb23d12a7e4ffb9437c3161a57fe2c2937d
Mikko De Los Reyes <mikkode212@gmail.com>
2024-07-17 10:40:35 -0400

    populated all countries by fetching data and displaying all flags and names of countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -13,21 +13,30 @@ const URL = "https://restcountries.com/v3.1/all"
 
 function Countries() {
 
-  const [country, setCountry] = useState("");
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch(URL)
-      console.log(result.json());
+      const result = await fetch(URL);
+      const data = await result.json();
+      setCountries(data);
     }
     fetchData();
-  })
+  },[])
 
   return (
     <div className='container'>
       <SearchBar/>
       <Dropdown className='dropdown' options={options} placeholder="Select an option"/>
-
+      <div className='countries-grid'>
+        {countries.map((country) => (
+          <div className='country-card' key={country.cca3}>
+            <img src={country.flags.png} alt={country.name.common} className="country-flag"/>
+            <h3>{country.name.common}</h3>
+            <p>{country.reigon}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
