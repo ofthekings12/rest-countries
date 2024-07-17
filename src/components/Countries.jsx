5b7128fdb06f30b22d85ce15b6a09df31c1bcd21
Mikko De Los Reyes <mikkode212@gmail.com>
2024-07-17 11:14:30 -0400

    arranged all countries into cards and into grid formation

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react';
-import './Countries.css';
-import './SearchBar'
-import SearchBar from './SearchBar';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
+import { useEffect, useState } from "react";
+import "./Countries.css";
+import "./SearchBar";
+import SearchBar from "./SearchBar";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
-const options = [
-  'one', 'two', 'three'
-];
+const options = ["one", "two", "three"];
 
-const URL = "https://restcountries.com/v3.1/all"
+const URL = "https://restcountries.com/v3.1/all";
 
 function Countries() {
-
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
@@ -20,25 +17,35 @@ function Countries() {
       const result = await fetch(URL);
       const data = await result.json();
       setCountries(data);
-    }
+    };
     fetchData();
-  },[])
+  }, []);
 
   return (
-    <div className='container'>
-      <SearchBar/>
-      <Dropdown className='dropdown' options={options} placeholder="Select an option"/>
-      <div className='countries-grid'>
+    <>
+    <div className="container">
+      <SearchBar />
+      <Dropdown
+        className="dropdown"
+        options={options}
+        placeholder="Select an option"
+      /> 
+      </div>
+      <div className="countries-grid">
         {countries.map((country) => (
-          <div className='country-card' key={country.cca3}>
-            <img src={country.flags.png} alt={country.name.common} className="country-flag"/>
+          <div className="country-card" key={country.cca3}>
+            <img
+              src={country.flags.png}
+              alt={country.name.common}
+              className="country-flag"
+            />
             <h3>{country.name.common}</h3>
             <p>{country.reigon}</p>
           </div>
         ))}
       </div>
-    </div>
-  )
+      </>
+  );
 }
 
 export default Countries;
