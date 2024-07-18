24c24a011f68598f7dc6e3167468e8345e1fd86c
Mikko De Los Reyes <mikkode212@gmail.com>
2024-07-18 11:29:44 -0400

    adjusted api to filter countries by region, dropdown now lists regions and filters and displays country by region

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,21 +5,35 @@ import SearchBar from "./SearchBar";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
-const options = ["one", "two", "three"];
+
 
 const URL = "https://restcountries.com/v3.1/all";
 
 function Countries() {
   const [countries, setCountries] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch(URL);
       const data = await result.json();
       setCountries(data);
-    };
+      const uniqueRegions = [...new Set(data.map(country => country.region))];
+      setRegions(uniqueRegions)
+    }
     fetchData();
   }, []);
+
+  const handleDropdownChange = (option) => {
+    setSelectedRegion(option.value);
+  }
+
+  const filteredCountries = selectedRegion ? countries.filter(country => country.region === selectedRegion) : countries;
+
+
+
+// const options = ["one", "two", "three"];
 
   return (
     <>
@@ -27,12 +41,13 @@ function Countries() {
       <SearchBar />
       <Dropdown
         className="dropdown"
-        options={options}
+        options={regions}
+        onChange={handleDropdownChange}
         placeholder="Select an option"
       /> 
       </div>
       <div className="countries-grid">
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <div className="country-card" key={country.cca3}>
             <img
               src={country.flags.png}
